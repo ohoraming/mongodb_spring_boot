2d70c9c851b9dca3fd798704a86d70fbf806ad59
ohoraming <kminsmile@gmail.com>
2022-08-02 16:55:15 +0900

    get/post/delete mapping 완료

---
+++ README.md
@@ -0,0 +1,15 @@
+## Spring Initializr 선택 옵션
+- Project
+    - Maven Project
+- Language
+    - Java
+- Spring Boot
+    - 2.7.2
+- Project Metadata
+    - Packaging: Jar
+    - Java: 17
+- Dependencies
+    - Spring Web
+    - Lombok
+    - Spring Data MongoDB
+[Spring Initializr Share](https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.7.2&packaging=jar&jvmVersion=17&groupId=com.example&artifactId=mongodb&name=mongodb&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.mongodb&dependencies=web,lombok,data-mongodb)
---
+++ src/main/java/com/example/mongodb/controller/ProductController.java
@@ -0,0 +1,66 @@
+package com.example.mongodb.controller;
+
+import com.example.mongodb.model.Product;
+import com.example.mongodb.repository.ProductRepository;
+import com.example.mongodb.resource.ProductRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+//@AllArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+//    위에 @AllArgsConstructor 추가한 것과 같음
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<List<Product>> getAllProducts() {
+
+        return ResponseEntity.ok(this.productRepository.findAll());
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<Product> createProduct(@RequestBody ProductRequest productRequest) {
+
+        Product product = new Product();
+        product.setName(productRequest.getName());
+        product.setDescription(productRequest.getDescription());
+
+        return ResponseEntity.status(201).body(this.productRepository.save(product));
+
+    }
+
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity getAllProductById(@PathVariable String id) {
+
+        Optional<Product> product = this.productRepository.findById(id);
+
+        if(product.isPresent()) {
+            return ResponseEntity.ok(product.get());
+        } else {
+            return ResponseEntity.ok("The product with id: " + id + " was not found.");
+        }
+    }
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity deleteProductById(@PathVariable String id) {
+
+        Optional<Product> product = this.productRepository.findById(id);
+
+        if(product.isPresent()) {
+            this.productRepository.deleteById(id);
+            return ResponseEntity.ok("Success.");
+        } else {
+            return ResponseEntity.ok("The product with id: " + id + " was not found.");
+        }
+    }
+
+}
